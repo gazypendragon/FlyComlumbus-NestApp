7617ae41f132c73b7f00b1418b96725e9eed6ba2
gazy cool <ndefrutitus22@gmail.com>
2023-09-23 09:59:08 -0400

    updated code files using  terraform to deploy nestapp

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,10 +1,10 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "ogati-terraform-remote-state"
+    bucket         = "dev-terraform-remote-state-2023"
     key            = "flycolumbus-nestapp/terraform.tfstate"
     region         = "us-east-1"
-    profile        = "Code-build-user"
+    profile        = "terraform-Dev-user"
     dynamodb_table = "terraform-state-lock"
   }
 }
